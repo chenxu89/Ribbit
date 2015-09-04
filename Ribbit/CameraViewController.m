12fcca3176ccb944873134f1cb3a985f3d70e118
chenxu89 <675269196@qq.com>
2015-09-04 21:29:58 +0800

    4-2 viewDidLoad and the View Controller Lifecycle

--- Ribbit/CameraViewController.m
+++ Ribbit/CameraViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    //small bug: Camera keeps opening after taking a picture when code is placed in (void)viewWillAppear, unless tap the cancel button.
     self.imagePicker = [[UIImagePickerController alloc] init];
     self.imagePicker.delegate = self;
     self.imagePicker.allowsEditing = NO;
@@ -30,6 +36,7 @@
     self.imagePicker.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:self.imagePicker.sourceType];
     
     [self presentViewController:self.imagePicker animated:NO completion:nil];
+    
 }
 
 #pragma mark - Table view data source
@@ -44,6 +51,15 @@
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
     return 0;
+}
+
+#pragma mark - Image Picker Controller Delegate
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [self dismissViewControllerAnimated:NO completion:nil];
+    
+    //点击cancel按钮后，回到第一个tap页面，即inbox页面
+    [self.tabBarController setSelectedIndex:0];
 }
 
 @end
